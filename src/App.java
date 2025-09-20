0d2713f82c54851cd8903dcc9ba9c6b3b134d0d7
CarlottaPolito <carlottapolitowd@gmail.com>
2025-09-20 17:48:00 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Studente s1 = new Studente("Carlotta", "Polito", 35);
+
+        System.out.println(s1.studenteConcatenato());
+    }
+}
---
+++ src/Studente.java
@@ -0,0 +1,19 @@
+public class Studente {
+
+    private String nome;
+
+    private String cognome;
+
+    private int eta;
+
+    public Studente(String nome, String cognome, int eta){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    public String studenteConcatenato(){
+        return this.nome + " " + this.cognome + ", " + this.eta;
+    }
+
+}
